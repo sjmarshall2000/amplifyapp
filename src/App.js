f91a65624ee87341a04e4e72ea95b2bf177ed619
Stephen Marshall <stephenjohnmarshall2000@gmail.com>
2022-03-21 13:42:41 -0600

    added text to test live changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<p>
+	  I added a paragraph here
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
