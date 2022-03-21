4919d8cfa92b454c5730dd4222a7dd372693de85
Stephen Marshall <stephenjohnmarshall2000@gmail.com>
2022-03-21 13:44:59 -0600

    added text again to test live changes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. heya, I just edited this.
         </p>
-	<p>
-	  I added a paragraph here
-	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
